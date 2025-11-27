dc3c178babc29adf917f8ca013e1f43a3d59ae61
木村健治 <omusubi3mk2@gmail.com>
2025-11-27 12:59:52 +0000

    サーバーからのタイムアウト値は３００秒に設定してください。

--- next.config.ts
+++ next.config.ts
@@ -23,12 +23,16 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+      ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
     ],
+  },
+  serverActions: {
+    bodyTimeout: 300,
   },
 };
 
