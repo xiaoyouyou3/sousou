ddd7348f12a03b74390279463340eba3d077cc43
木村健治 <omusubi3mk2@gmail.com>
2025-11-27 15:01:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5100618064-77b13",
+  "appId": "1:510803276106:web:dd3257938af813f7d391b3",
+  "apiKey": "AIzaSyAWy7tAn03im3PvLoaCmnq7kCB5MYLzgko",
+  "authDomain": "studio-5100618064-77b13.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "510803276106"
+};
